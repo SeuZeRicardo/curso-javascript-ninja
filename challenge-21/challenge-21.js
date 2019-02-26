f253421106c01dd740ce6766dfb52e8fb1587670
Ricardo Carvalho <ricardo.carvalho@infracommerce.com>
2019-02-26 14:51:31 -0300

    Estudos do exercicio

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -14,4 +14,36 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-// ?
+document.addEventListener("DOMContentLoaded", function () {   
+  //Inicializa os valores dos minutos e dos campos selecionados
+  var showCounter = document.querySelector('.counter');
+  var countDownValue = document.querySelector('input').value;
+  var counter = 0;
+  var stopTimer;
+
+  //Função que faz o contador acrescentar de 1 em 1 a cada segundo
+  function timer(){
+    console.log('timer', counter++);
+    if(counter > countDownValue)
+      return;      
+      stopTimer = setTimeout(timer , 1000);
+      showCounter.innerHTML = counter;
+  }
+
+  //Quando clicado em cada botão chama a respectiva função
+  document.querySelector('[data-js="start-clock"]').addEventListener('click', function () {
+    timer();//Inicializa o contador
+  });
+
+  //A função clearTimeout faz que pare o contador, pois na função Timer() a variavel stopTimer esta recebendo a ID da função do setTimeout e acrescentando a cada segundo o contador
+  document.querySelector('[data-js="pause-clock"]').addEventListener('click', function () {
+    clearTimeout(stopTimer);
+  });  
+
+  document.querySelector('[data-js="reset-clock"]').addEventListener('click', function () {
+    // Função para parar o cronometro quando clicado e zerar o campo e o contador
+    clearTimeout(stopTimer);
+    counter = 0;
+    showCounter.innerHTML = counter;
+  });
+});
