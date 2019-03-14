3ddc04bf72dafce19e042f9ee3963a1334a8e1a5
Ricardo Carvalho <ricardo.carvalho@infracommerce.com>
2019-03-14 10:46:33 -0300

    Estudo e correção do exercicio 28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,6 @@
+(function () {
+  'use strict'
+
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +28,60 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  var $inputCEP = document.querySelector('[data-js="cep"]');
+  var $buttonSubmit = document.querySelector('[type="button"]');
+  var $textStatus = document.querySelector('.ajaxAnswer');
+  var ajax = new XMLHttpRequest();
+
+  //Verifica se  existe algum caracter que não seja numero
+  function getCEP(){
+    return $inputCEP.value.replace(/\D/g, '');
+  }
+  
+  //verifica se a requisição esta OK
+  function isRequestOk() {
+    return ajax.readyState === 4 && ajax.status === 200;
+  }
+
+  //Função para mostrar os valores que veio da API
+  function showData(){
+    var data = JSON.parse(ajax.responseText);
+    var $textInfo = document.querySelectorAll('p');
+    
+    $textInfo.forEach(function (value, key){          
+      switch (value.className) {
+        case 'logradouro':
+          return value.innerHTML = 'Logradouro: ' + data.logradouro +'';
+        case 'bairro':
+          return value.innerHTML = 'Bairro: ' + data.bairro +'';
+        case 'estado':
+          return value.innerHTML = 'Estado: ' + data.uf +'';
+        case 'cidade':
+          return value.innerHTML = 'Cidade: ' + data.localidade +'';
+        case 'cep':
+          return value.innerHTML = 'CEP: ' + data.cep +'';        
+      }      
+    });
+  }
+
+  function requestCEP() {
+    event.preventDefault();
+    var cepValue = getCEP();    
+    ajax.open('GET', "https://viacep.com.br/ws/" + cepValue + "/json/");    
+    ajax.send();
+    ajax.addEventListener('readystatechange', function () {
+      $textStatus.innerHTML ='Buscando informações para o CEP ' + $inputCEP.value + '...';      
+      if(isRequestOk()){
+        $textStatus.innerHTML = 'Endereço referente ao CEP ' + $inputCEP.value + ':';
+        showData();
+      }
+      else{
+        $textStatus.innerHTML = 'Não encontramos o endereço para o CEP ' + $inputCEP.value + '.';      
+      }      
+    });        
+  }
+
+  //Quando clicado chama a função de buscar na API
+  $buttonSubmit.addEventListener('click', requestCEP, false);
+})();
